bd93fc3f30e5c57592f4177e6d9b178c73685061
ibukamshindi <ibukamshindi@gmail.com>
2020-07-19 22:59:16 +0300

    this will add intial script

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function akanName (){
+  var year=parseInt(documennt.getElementById("year"));
+  
+  var month=parseInt(document.getElementById("month"));
+  
+  var day=parseInt(document.getElementById)("day"));
+  
+  var male=document.getElementbyId("male")
+  
+  var female=document.getElementbyId("female")
+  
+  }
